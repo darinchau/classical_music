5387494cc110f856379c5aee110cc2a77486f0a4
Darin Chau <darinchauyf@gmail.com>
2025-04-20 09:49:13 +0800

    add test files

---
+++ src/analyse/jsymbolic.py
@@ -0,0 +1,3 @@
+# Extracts features using jsymbolic
+# Get jsymbolic from https://sourceforge.net/projects/jmir/files/jSymbolic/jSymbolic%202.2/jSymbolic_2_2_user.zip/download
+# Extract the zip file and place the extracted directory inside resources/
